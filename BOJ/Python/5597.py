94bb484f9ca9012b3155b1f5c5d645df3498d3d3
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2022-07-26 02:35:57 +0900

    Update 5597.py

--- BOJ/Python/5597.py
+++ BOJ/Python/5597.py
@@ -5,17 +5,11 @@ stu2 = []
 for i in range(28):
     n = int(input())
     stu.append(n)
-stu.append(0)
-stu.append(0)
-
 for i in range(30):
     stu1.append(i+1)
-
-for i in range(30):
-    if stu[i] in stu:
-        pass
-    else:
+for i in stu1:
+    if i not in stu:
         stu2.append(i)
-print(stu)
-print(stu1)
-print(stu2)
+stu2.sort()
+print(stu2[0])
+print(stu2[1])
