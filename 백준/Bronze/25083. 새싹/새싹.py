07d35afca830ef10fc394b6f0e1eb17999e9ec26
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2022-08-02 16:44:21 +0900

    [Bronze V] Title: 새싹, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,6 @@
+print('         ,r', end="");print("'",end="");print('"7')
+print("r`-_   ,'  ,/")
+print(' \. ". L_r', end=""); print("'")
+print('   `~', end=""); print('\\', end=""); print('/')
+print('      |')
+print('      |')
