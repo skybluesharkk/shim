3da7fa199de7b8d76506cdae506f4040d48b98e0
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2022-08-25 03:26:41 +0900

    [Bronze IV] Title: Archivist, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/20232. Archivist/Archivist.py
@@ -0,0 +1,54 @@
+a = int(input())
+
+if (a==1995):
+    print('ITMO')
+elif (a==1996):
+    print('SPbSU')
+elif (a==1997):
+    print('SPbSU')
+elif (a==1998):
+    print('ITMO')
+elif (a==1999):
+    print('ITMO')
+elif (a==2000):
+    print('SPbSU')
+elif (a==2001):
+    print('ITMO')
+elif (a==2002):
+    print('ITMO')
+elif (a==2003):
+    print('ITMO')
+elif (a==2004):
+    print('ITMO')
+elif (a==2005):
+    print('ITMO')
+elif (a==2006):
+    print('PetrSU, ITMO')
+elif (a==2007):
+    print('SPbSU')
+elif (a==2008):
+    print('SPbSU')
+elif (a==2009):
+    print('ITMO')
+elif (a==2010):
+    print('ITMO')
+elif (a==2011):
+    print('ITMO')
+elif (a==2012):
+    print('ITMO')
+elif (a==2013):
+    print('SPbSU')
+elif (a==2014):
+    print('ITMO')
+elif (a==2015):
+    print('ITMO')
+elif (a==2016):
+    print('ITMO')
+elif (a==2017):
+    print('ITMO')
+elif (a==2018):
+    print('SPbSU')
+elif (a==2019):
+    print('ITMO')
+else:
+    pass
