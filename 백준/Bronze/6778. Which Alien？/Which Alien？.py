c6db94cedac2f09ada4f76bf52fa0710ad2c7837
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2022-08-27 23:01:12 +0900

    [Bronze IV] Title: Which Alien?, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,9 @@
+antena = int(input())
+eye = int(input())
+
+if (antena>=3) and (eye<=4):
+    print('TroyMartian')
+if (antena<=6) and (eye>=2):
+    print('VladSaturnian')
+if (antena<=2) and (eye<=3):
+    print('GraemeMercurian')
