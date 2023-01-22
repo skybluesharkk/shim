e74d5730a08d4e2e86918a4993aba4f22aab2ecf
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2023-01-22 22:06:00 +0900

    [Bronze III] Title: 알람 시계, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -0,0 +1,17 @@
+h,m = map(int,input().split())
+
+if (h==0)and(m!=0):
+    h=24
+elif (h==0)and(m==0):
+    h=23
+    m=60
+else:
+    pass
+totaltime = h*60+m
+#print(h,m)
+#print(totaltime)
+totaltime-=45
+if ((int(totaltime/60))!=24):
+    print(int(totaltime/60),totaltime%60)
+else:
+    print(0, totaltime % 60)
