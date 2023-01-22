be836cf4117164e4357ea40f9ad95f4bd988743c
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2023-01-22 21:52:46 +0900

    Update 단어의 개수.py

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -2,8 +2,6 @@ import sys
 
 a = sys.stdin.readline()
 
-#print(a)
-#print(type(a))
 b = list(a)
 b.remove('\n')
 if (len(b)==1) and (b[0]==' '):
@@ -18,21 +16,12 @@ else:
     if (len(b)==1):
         print(1)
     else:
-        #print(b)
         cnt=0
-        #c=''
-        #d=[]
         for i in range(len(b)):
             if (i==0):
                 pass
-            #if (i==len(b)):
-             #   cnt+=1
-              #  d.append(c)
             if (((b[i-1]!=" ")and (b[i]==' '))or i==len(b)-1):
                 cnt+=1
-                #d.append(c)
-                #c=''
             else:
                 pass
-
         print(cnt)
