f791d88e6fa4425fe337a4619077a7021d1cd2c5
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2023-01-27 03:01:31 +0900

    [Bronze I] Title: N번째 큰 수, Time: 156 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2693. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,10 @@
+n = int(input())
+for i in range(n):
+    a = list(map(int,input().split()))
+    for i in range(len(a)):
+        for j in range(len(a)-1-i):
+            if (a[j]<=a[j+1]):
+                tmp=a[j]
+                a[j]=a[j+1]
+                a[j+1]=tmp
+    print(a[2])
