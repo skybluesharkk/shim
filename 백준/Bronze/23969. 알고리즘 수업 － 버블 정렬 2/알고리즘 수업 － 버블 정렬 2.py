fb0c220c31f09d3f9a08d7c30f8f4dd6a04ffcab
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2023-02-07 19:52:18 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 2, Time: 360 ms, Memory: 115412 KB -BaekjoonHub

---
+++ 백준/Bronze/23969. 알고리즘 수업 － 버블 정렬 2/알고리즘 수업 － 버블 정렬 2.py
@@ -0,0 +1,17 @@
+import sys
+
+n,k = map(int, sys.stdin.readline().split())
+ans = list(map(int, sys.stdin.readline().split()))
+answer = -1
+cnt=0
+y = len(ans)
+
+for i in range(y-1):
+    for j in range(y-1-i):
+        if (ans[j]>ans[j+1]):
+            ans[j], ans[j+1] = ans[j+1],ans[j]
+            cnt+=1
+            if (cnt==k):
+                print(' '.join(list(map(str,ans))))
+                exit()
+print(answer)
