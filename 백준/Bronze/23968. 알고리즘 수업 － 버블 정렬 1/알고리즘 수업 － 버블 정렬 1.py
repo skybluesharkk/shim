7fd123cbfe18f4b08899e8a7833c75557090804c
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2023-02-07 19:40:53 +0900

    [Bronze I] Title: 알고리즘 수업 - 버블 정렬 1, Time: 376 ms, Memory: 115412 KB -BaekjoonHub

--- 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
+++ 백준/Bronze/23968. 알고리즘 수업 － 버블 정렬 1/알고리즘 수업 － 버블 정렬 1.py
@@ -1,23 +1,17 @@
 import sys
-n,k = map(int,input().split())
 
+n,k = map(int, sys.stdin.readline().split())
 ans = list(map(int, sys.stdin.readline().split()))
-
-
+answer = -1
 cnt=0
 y = len(ans)
 for i in range(y-1):
-    change = False
+
     for j in range(y-1-i):
         if (ans[j]>ans[j+1]):
             ans[j], ans[j+1] = ans[j+1],ans[j]
             cnt+=1
-            change = True
-            if (cnt==k):
-                print(ans[j],ans[j+1])
-                break
-    if (cnt==k) or (change==False):
-        break
 
-if (cnt<k):
-    print(-1)
+            if (cnt==k):
+                answer = f'{ans[j]} {ans[j+1]}'
+print(answer)
