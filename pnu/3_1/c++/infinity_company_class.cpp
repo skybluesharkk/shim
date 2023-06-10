f76ef0c36424ff847118ffc8b761656bb8edd025
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2023-06-10 18:12:51 +0900

    Update infinity_company_class.cpp

--- pnu/3_1/c++/infinity_company_class.cpp
+++ pnu/3_1/c++/infinity_company_class.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 class Employee {
+protected:
     string name;
     int age;
     string position;
@@ -11,7 +12,7 @@ class Employee {
 
 public:
     Employee(string name,int age, string position, int rank):
-    name(name),age(age),position(position),rank(rank){}
+            name(name),age(age),position(position),rank(rank){}
 
     Employee(const Employee& employee) {
         name = employee.name;
@@ -22,29 +23,54 @@ public:
 
     Employee() {}
 
-    void print_info() {
+    virtual void print_info() {
         cout<<name<<position<<age<<calculate_pay()<<endl;
     }
-    int calculate_pay() {
+    virtual int calculate_pay() {
         return 200 + rank * 50;
+    }
+};
+
+class Manager : public Employee {
+    int year_of_service;
+
+public:
+    Manager(string name, int age,string position,int rank,int year_of_service)
+    : Employee(name,age,position,rank),year_of_service(year_of_service) {}
+
+    Manager(const Manager& manager)
+    : Employee(manager.name,manager.age,manager.position,manager.rank){
+        year_of_service = manager.year_of_service;
+    }
+
+    Manager() : Employee() {}
+
+    int calculate_pay() override { return 200 + rank*50+5*year_of_service;}
+    void print_info() override {
+        cout<<name<<position<<age<<year_of_service<<calculate_pay()<<endl;
+
     }
 };
 
 class EmployeeList {
     int alloc_employee;
+
     int current_employee;
+
     Employee** employee_list;
+
 
 public:
     EmployeeList(int alloc_employee) : alloc_employee(alloc_employee){
         employee_list = new Employee * [alloc_employee];
-        current_employee =0;
+        current_employee = 0;
     }
 
     void add_employee(Employee* employee){
         employee_list[current_employee] = employee;
         current_employee++;
     }
+
     int current_employee_num(){ return current_employee; }
 
     void print_employee_info() {
@@ -66,12 +92,11 @@ int main(){
     EmployeeList emp_list(10);
     emp_list.add_employee(new Employee("노홍철",34,"평사원",1));
     emp_list.add_employee(new Employee("하하",34,"평사원",1));
-    emp_list.add_employee(new Employee("유재석",41,"부장",7));
-    emp_list.add_employee(new Employee("정준하",43,"과장",4));
-    emp_list.add_employee(new Employee("박명수",43,"차장",5));
+    emp_list.add_employee(new Manager("유재석",41,"부장",7,12));
+    emp_list.add_employee(new Manager("정준하",43,"과장",4,15));
+    emp_list.add_employee(new Manager("박명수",43,"차장",5,13));
     emp_list.add_employee(new Employee("정형돈",36,"대리",2));
     emp_list.add_employee(new Employee("길",36,"인턴",-2));
     emp_list.print_employee_info();
     return 0;
 }
-
