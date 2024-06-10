545729f6003a8840059b7832dd9105dddde99be2
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2024-06-10 18:28:51 +0900

    [Silver V] Title: 집합, Time: 3752 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.py
+++ 백준/Silver/11723. 집합/집합.py
@@ -1,26 +1,26 @@
 import sys
-
-# 초기화
 sets = set()
 num = int(input())
+# all,empty가 문제네. 무지성으로 넣으면 안되고 두개는 따로 해야 하는데.
 
-for _ in range(num):
-    ori_order = sys.stdin.readline().strip()
-
-    if ori_order == 'all':
-        sets = set(range(1, 21))
-    elif ori_order == 'empty':
+for i in range(num):
+    ori_order = (sys.stdin.readline().strip())
+    if ori_order=='all':
+        sets = set(range(1,21))
+    elif ori_order=='empty':
         sets.clear()
     else:
-        order, x = ori_order.split()
+        order,x = ori_order.split()
         x = int(x)
-
         if order == 'add':
             sets.add(x)
         elif order == 'remove':
-            sets.discard(x)  # discard는 존재하지 않아도 에러가 발생하지 않음
+            sets.discard(x)
         elif order == 'check':
-            print(1 if x in sets else 0)
+            if x in sets:
+                print(1)
+            else:
+                print(0)
         elif order == 'toggle':
             if x in sets:
                 sets.remove(x)
