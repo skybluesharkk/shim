524931841b00ab671110fd067e20e2c1905f3c6e
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-01-13 02:02:14 +0900

    [Silver IV] Title: 설탕 배달, Time: 280 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -2,8 +2,8 @@ n = int(input())
 
 # 5x+3y=18
 ans = []
-for i in range(n):
-    for j in range(n):
+for i in range(n//5 +1):
+    for j in range(n //3 +1):
         if (5*i+3*j)==n:
             ans.append(i+j)
 if len(ans)==0:
